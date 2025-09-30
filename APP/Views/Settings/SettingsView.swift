6b6da0747e6b6b56c101aa161a834d0473b75419
end1essrain <110792623+end1essrain@users.noreply.github.com>
2025-09-30 10:12:46 +0800

    Update SettingsView.swift

--- APP/Views/Settings/SettingsView.swift
+++ APP/Views/Settings/SettingsView.swift
@@ -3,7 +3,7 @@ import UIKit
 import Darwin
 
 struct SettingsView: View {
-    private let _githubUrl = "https://github.com/pxx917144686/APP"
+    private let _githubUrl = "https://baidu.com/"
     @State private var currentIcon = UIApplication.shared.alternateIconName
     @StateObject private var optionsManager = OptionsManager.shared
     
@@ -24,22 +24,22 @@ struct SettingsView: View {
 
 extension SettingsView {
     @ViewBuilder
-    private func _feedback() -> some View {
-        Section {
-            Button("提交反馈", systemImage: "safari") {
-                if let url = URL(string: "\(_githubUrl)/issues") {
-                    UIApplication.shared.open(url)
-                }
-            }
-            Button("👉看看源代码", systemImage: "safari") {
-                if let url = URL(string: _githubUrl) {
-                    UIApplication.shared.open(url)
-                }
-            }
-        } footer: {
-            Text("有任何问题，或建议，请随时提交。")
-        }
-    }
+  //  private func _feedback() -> some View {
+       // Section {
+          //  Button("提交反馈", systemImage: "safari") {
+            //    if let url = URL(string: "\(_githubUrl)/issues") {
+               //     UIApplication.shared.open(url)
+             //   }
+          //  }
+          //  Button("看看源代码", systemImage: "safari") {
+              //  if let url = URL(string: _githubUrl) {
+                 //   UIApplication.shared.open(url)
+             //   }
+          //  }
+      //  } footer: {
+         //   Text("有任何问题，或建议，请随时提交。")
+     //   }
+  //  }
 
     private var appearanceSection: some View {
         Section {
